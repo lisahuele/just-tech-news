55f130ab7a0a5b0510eee37e4771a3a4948594fc
lisahuele <lisahuele@gmail.com>
2021-09-04 13:53:27 -0400

    finish

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
 const { Post, User, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 
 //GET all posts
@@ -83,11 +84,11 @@ router.get('/:id', (req, res) => {
 })
 
 //Create a post
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
-        user_id: req.body.user_id
+        user_id: req.session.user_id
     })
     .then(dbPostData => res.json(dbPostData))
     .catch(err => {
@@ -97,7 +98,7 @@ router.post('/', (req, res) => {
 })
 
 //PUT /api/post/upvote
-router.put('/upvote', (req, res) => {
+router.put('/upvote', withAuth, (req, res) => {
     // make sure the session exists first
     if (req.session) {
       // pass session id along with all destructured properties on req.body
@@ -111,7 +112,7 @@ router.put('/upvote', (req, res) => {
   });
 
 //Update a post
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update(
         {
             title: req.body.title
@@ -135,7 +136,7 @@ router.put('/:id', (req, res) => {
 })
 
 //Delete a post
-router.delete('/:id', (req, res) => {
+router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
         where: {
             id: req.params.id
--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Vote, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 //GET  api/users
 router.get('/', (req, res) =>  {
@@ -55,7 +56,7 @@ router.get('/:id', (req, res) => {
 });
 
 //POST api/users
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     User.create({
         username: req.body.username,
         email: req.body.email,
@@ -104,7 +105,7 @@ router.post('/login', (req, res) => {
 });
 
 //Log out
-router.post('/logout', (req, res) => {
+router.post('/logout', withAuth, (req, res) => {
     if(req.session.loggedIn) {
         req.session.destroy(() => {
             res.status(204).end();
@@ -115,7 +116,7 @@ router.post('/logout', (req, res) => {
 })
 
 //PUT api/users/1
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     User.update(req.body, {
         individualHooks: true,
         where: {
@@ -136,7 +137,7 @@ router.put('/:id', (req, res) => {
 });
 
 //DELETE api/user/1
-router.delete('./:id', (req, res) => {
+router.delete('./:id', withAuth, (req, res) => {
     User.destroy({
         where: {
             id: req.params.id
